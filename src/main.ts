19fb63a01cb0f46ae8203593c420b634995955cc
Chris Coudron <coudron@gmail.com>
2022-05-24 19:58:13 -0400

    Update that gets all the open positions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { AnchorProvider, Program } from '@project-serum/anchor'
 import { Wallet } from '@project-serum/anchor'
-import { Connection, Keypair, PublicKey } from '@solana/web3.js'
+import { Connection, Keypair } from '@solana/web3.js'
+import { HEDGE_PROGRAM_PUBLICKEY, IDL, Vault } from 'hedge-web3'
 
-const HEDGE_ADDRESS = new PublicKey(
-  'HedgeEohwU6RqokrvPU4Hb6XKPub8NuKbnPmY7FoMMtN',
-)
+import base58 from 'bs58'
 
 async function hello() {
   console.log('Hello Notifi')
@@ -18,11 +17,20 @@ async function attach() {
   const provider = new AnchorProvider(finalizeConnection, wallet, {
     commitment: 'confirmed',
   })
-  const program = await Program.at(HEDGE_ADDRESS, provider)
+  const program = new Program<Vault>(IDL, HEDGE_PROGRAM_PUBLICKEY, provider)
 
   program.addEventListener('DepositVaultEvent', (event) => {
     console.log('Hey Notifi, this vault had a deposit:', event)
     console.log(JSON.stringify(event))
+    fetchOpenStabilityPoolPositions(program).then((positions) => {
+      positions.forEach((position) => {
+        console.log(
+          'Send a notification to:',
+          position.account.ownerAccount.toString(),
+        )
+      })
+      console.log(`Attempted to notifi ${positions.length} people.`)
+    })
   })
   program.addEventListener('LiquidateVaultEvent', (event) => {
     console.log('Hey Notifi, this vault was liquidated:', event)
@@ -30,5 +38,17 @@ async function attach() {
   })
 }
 
+async function fetchOpenStabilityPoolPositions(program: Program<Vault>) {
+  const positions = await program.account.liquidationPosition.all([
+    // Filter on Open positions: 1_u64
+    {
+      memcmp: {
+        bytes: base58.encode(Buffer.from([0])),
+        offset: 8 + 32 * 2 + (16 * (128 * 2 + 3) + 8 * 4),
+      },
+    },
+  ])
+  return positions
+}
 hello()
 attach()
